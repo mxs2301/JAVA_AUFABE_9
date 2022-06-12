5c38f5ed5c9024226f17d4131151307a54f15cf6
Max Schopf <max.schopf@mailfence.com>
2022-06-12 14:11:12 +0200

    Woerterbuchdateien Anhang

---
+++ src/main/java/com/maxschopf/Woerterbuch.java
@@ -0,0 +1,23 @@
+package com.maxschopf;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Woerterbuch{
+
+	public static class TreeMapTest{
+
+		public static void print(){
+			Map<Integer, String> map = new TreeMap<>();
+			map.put(42, "Datenstruktur");
+			map.put(8, "und");
+			map.put(5, "Algorithmen");
+
+			for(String eintrag: map.values()){
+				System.out.println(eintrag);
+			}
+		}
+
+	}
+
+}
